14b6c8de2da619eab9961e2db973060770d8269f
LoganLieou <logan.lieou@gmail.com>
2022-01-10 04:04:43 -0600

    darn

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import xgboost as xgb
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
 
-training = pd.read_excel("./data/training.xlsx")
+# read in the training data then split into labels and features
+training = pd.read_excel("./data/training.xlsx", sheet_name="Full information sample")
+training = training.dropna(axis="columns") # there appeared to exist NA values
 params = training.drop(["Observed GWP (assessed)"], axis=1)
 labels = training["Observed GWP (assessed)"]
 
-X_train, X_test, y_train, y_test = train_test_split(params, labels, test_size=0.2)
+# output the number of entries the training set has
+print("\ntraining dataset has: {0} entries".format(len(training)))
 
-# Hyper Parameter Optimization
-params = {"objective": "reg:squarederror",
-          "max_depth": [0:10],
-          "colsample_bylevel": 0.5,
-          "learning_rate": [0.01:0.1:0.01],
-          "random_state": [1:20]}
+# Just fit the regression model
+reg = RandomForestRegressor()
+reg.fit(params, labels)
 
-# Data
-data = xgb.DMatrix(data=X_train, label=y_train)
-cv_results = xgb.cv(dtrain)
+# Testing
+testing = pd.read_excel("./data/testing.xlsx", sheet_name="Full information sample")
+testing = testing.dropna(axis="columns")
+testing_params = training.drop(["Observed GWP (assessed)"], axis=1)
+testing_labels = training["Observed GWP (assessed)"]
 
-# Using XGBoost for regression
-reg = xgb.XGBRegressor()
-reg.fit(X_train, y_train)
-print("XGBoost: ", reg.score(X_test, y_test))
+# output information on parameters as well as test set accuracy
+print("\nLIST OF PARAMETERS: \n")
+for param in testing_params.columns: print(param)
+print("\nTEST ACCURACY: ", reg.score(testing_params, testing_labels), "\n")
 
+# Validation Set (this is sort of sus)
+validation = pd.read_excel("./data/validation.xlsx", sheet_name="Full information sample")
+validation = validation.dropna(axis="columns")
+v_params = validation.drop(["Observed GWP (assessed)"], axis=1)
+v_labels = validation["Observed GWP (assessed)"]
